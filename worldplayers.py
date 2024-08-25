f81ebf24de002603ce1a0d5b2d248facdb235097
DragonInspire <140202563+DragonInspire@users.noreply.github.com>
2024-08-24 23:15:23 -0400

    Update worldplayers.py

--- worldplayers.py
+++ worldplayers.py
@@ -29,5 +29,16 @@ async def world_players(world):
 
         players_data.append([player, guild, highest_level_name, highest_level])
 
+    null_sorted_list
+    for player in player_data:
+        if player_data[1] == "none":
+            null_sorted_list.append(player)
+
+    null_sorted_list = sorted(null_sorted_list, key = lambda player: player[3])
+
+     for player in player_data:
+        if player_data[1] != "none":
+            null_sorted_list.append(player)
+
 
     return players_data
