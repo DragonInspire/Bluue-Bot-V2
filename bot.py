876b6cbefff7cc7fb1835252983ad19e24d882ad
DragonInspire <140202563+DragonInspire@users.noreply.github.com>
2024-08-24 23:39:49 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -372,15 +372,12 @@ async def wc(interaction: discord.Interaction, world: str):
     message = f" \t  \t  Players online on WC{world}"
     
     for player in the_world_players:
-        message += "\n" + str(player[0]) + " "*(18-len(player[0])) + " " + str(player[1]) + " "*(30-len(player[1]))  + str(player[2]) + " "*(10-len(player[2])) + str(player[3]) + " "*(4-len(player[3]))  + str(player[4]) + " "*(10-len(player[4])) +  str(player[5])
-    if len(message) > 2000:
-        buffer = StringIO(message)
-        f = discord.File(buffer, filename="playersonline.txt")
+        message += "\n" + str(player[0]) + " "*(18-len(player[0])) + " " + str(player[1]) + " "*(30-len(player[1]))  + str(player[2]) + " "*(10-len(player[2])) + str(player[3]) + " "*(4-len(str(player[3])))  + str(player[4]) + " "*(10-len(player[4])) +  str(player[5])
+
+    buffer = StringIO(message)
+    f = discord.File(buffer, filename="playersonline.txt")
         
-        await channel.send(file = f)
-        return
-        
-    await channel.send(message)
+    await channel.send(file = f)
         
 
 # Background task: Fetch and display online players
