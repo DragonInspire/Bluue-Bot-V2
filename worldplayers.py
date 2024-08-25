7621a23e7e6eb45a7e8e42510bc30dad3e2ff2f0
DragonInspire <140202563+DragonInspire@users.noreply.github.com>
2024-08-24 23:17:18 -0400

    Update worldplayers.py

--- worldplayers.py
+++ worldplayers.py
@@ -35,8 +35,7 @@ async def world_players(world):
             null_sorted_list.append(player)
 
     null_sorted_list = sorted(null_sorted_list, key = lambda player: player[3])
-
-     for player in player_data:
+    for player in player_data:
         if player_data[1] != "none":
             null_sorted_list.append(player)
 
