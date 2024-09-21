9f5fa65f2819bb53a068876b83bebda54d093b85
DragonInspire <140202563+DragonInspire@users.noreply.github.com>
2024-09-21 14:39:40 -0400

    Fix XP leaderboard

--- src/bot.py
+++ src/bot.py
@@ -609,7 +609,7 @@ async def war_update():
 @tasks.loop(minutes=1)
 async def xp_leaderboard():
     now = datetime.now()
-    if now.hour != 24:
+    if now.hour != 0:
         return
     if now.minute != 0:
         return
