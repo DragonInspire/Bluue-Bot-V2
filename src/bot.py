4e10100f19f6d5716c9bc90136b8e8024e899769
DragonInspire <140202563+DragonInspire@users.noreply.github.com>
2024-09-29 14:04:18 -0400

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -435,7 +435,7 @@ async def timeline(interaction: discord.Interaction):
     await interaction.response.send_message("https://docs.google.com/document/d/1KtCntwPyaEaaEe8VVc3a-_pmuV6argoi2m2gun53EDE/edit")
 
 @farplane_group.command(name="signature")
-async def signature(interaction: discord.Interaction, year: str):
+async def signature(interaction: discord.Interaction):
     years = {"2020": "https://media.discordapp.net/attachments/1289229656666406924/1290001922547257405/farplane2020.gif?ex=66fadf69&is=66f98de9&hm=0804ae573967656f0ee42fd82f89acc3a03bebf6e05c11cc0fe5e7cab71d8807&=",
             "2021": "https://media.discordapp.net/attachments/1289229656666406924/1290001945938886770/farplane2021.gif?ex=66fadf6e&is=66f98dee&hm=c693de21c6d1fec40b06cfa3245d407ddbf767e8cf567a8199cb6d93fbde6b53&=",
             "2022": "https://media.discordapp.net/attachments/1289229656666406924/1290001969695555644/farplane2022signature.gif?ex=66fadf74&is=66f98df4&hm=c44a6dc6ad96acae13c8ece0c8cc35bfbc4b3ece750916297a01ebe691189f92&=",
